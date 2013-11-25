56b03863eefe93f185af4e993a58d37883dbf2af
Nam Sy Vo <vosynam@gmail.com>
2013-11-25 04:33:34 -0600

    Change function base_cmp to map Discard Interface implementation Rewrite EditDistance function, and allow early break Remove redundant code

--- distance.go
+++ distance.go
@@ -1,314 +1,242 @@
 /*
-ed package provides some functions for generalized edit-distance calculation.
+Distance package provides some functions for Hamming and edit-distance calculation.
 
-String calculates the standard edit-distance, and StringFull returns an extra longest-common-string.
+EditDistanceStr calculates the standard edit-distance.
 
-EditDistance calculates the generalized edit-distance, and EditDistanceFull returns extra matching infomation. Base is a helper type using as a base type for Interface implementations.
-
-EditDistanceFunc and EditDistanceFuncFull are similar to EditDistance and EditDistanceFull, but using parameter and functions instead of an interface. This is sometimes more easy to use. ConstCost is a helper function.
+EditDistance calculates the edit-distance for multi-genomes,
+ and EditDistanceFull returns extra matching infomation.
 */
+
 package Distance
 
 import (
-	"unicode/utf8"
-	"bytes"
-//    "fmt"
+	"math"
+    "fmt"
 )
 
-var A_in = []byte{'A', 'R', 'M', 'W', 'D', 'H', 'V', 'N'} 
-var C_in = []byte{'C', 'Y', 'M', 'S', 'B', 'H', 'V', 'N'} 
-var G_in = []byte{'G', 'R', 'K', 'S', 'B', 'D', 'V', 'N'} 
-var T_in = []byte{'T', 'Y', 'K', 'W', 'B', 'D', 'H', 'N'}
-
-func base_cmp(a, b byte) bool {
-	switch b {
-	case 'A':
-		if bytes.IndexByte(A_in, a) != -1 {
-			return true
-		}
-		return false
-	case 'C':
-		if bytes.IndexByte(C_in, a) != -1 {
-			return true
-		}
-		return false
-	case 'G':
-		if bytes.IndexByte(G_in, a) != -1 {
-			return true
-		}
-		return false
-	case 'T':
-		if bytes.IndexByte(T_in, a) != -1 {
-			return true
-		}
-		return false
-	default:
-		return false
-	}
+var A_to_iupac = map[byte]bool{
+    'A': true, 'R': true, 'M': true, 'W': true, 'D': true, 'H': true, 'V': true, 'N': true,
+    'a': true, 'r': true, 'm': true, 'w': true, 'd': true, 'h': true, 'v': true, 'n': true,
 }
 
-func min(a, b int) int {
+var C_to_iupac = map[byte]bool{
+    'C': true, 'Y': true, 'M': true, 'S': true, 'B': true, 'H': true, 'V': true, 'N': true,
+    'c': true, 'y': true, 'm': true, 's': true, 'b': true, 'h': true, 'v': true, 'n': true,
+}
+
+var G_to_iupac = map[byte]bool{
+    'G': true, 'R': true, 'K': true, 'S': true, 'B': true, 'D': true, 'V': true, 'N': true,
+    'g': true, 'r': true, 'k': true, 's': true, 'b': true, 'd': true, 'v': true, 'n': true,
+}
+var T_to_iupac = map[byte]bool{
+    'T': true, 'Y': true, 'K': true, 'W': true, 'B': true, 'D': true, 'H': true, 'N': true,
+    't': true, 'y': true, 'k': true, 'w': true, 'b': true, 'd': true, 'h': true, 'n': true,
+}
+
+var std_to_iupac = map[byte]map[byte]bool{
+    'A': A_to_iupac,
+    'C': C_to_iupac,
+    'G': G_to_iupac,
+    'T': T_to_iupac,
+}
+
+var snp_pos = map[int]int{
+	1:100,
+	5:500,
+	10:1000,
+	15:1500,
+}
+
+func PrintMaps(){
+	fmt.Println(A_to_iupac)
+	fmt.Println(C_to_iupac)
+	fmt.Println(G_to_iupac)
+	fmt.Println(T_to_iupac)
+	fmt.Println(std_to_iupac)
+}
+
+var r_e float32 = 0.1
+var r_m float32 = 0.01
+var r_x float32 = 0.05
+var infty float32 = 1000
+
+var snp_sub_cost float32 = float32(math.Log10(float64(1/r_e)) + math.Log10(float64(1/r_m)))
+var non_snp_sub_cost float32 = float32(math.Log10(float64(1/r_e)))
+
+var Dist_Threshold int = 100
+
+func minInt(a, b int) int {
 	if a < b {
 		return a
 	} // if
-
 	return b
 }
 
+func maxInt(a, b int) int {
+	if a > b {
+		return a
+	} // if
+	return b
+}
+
+func minFloat32(a, b float32) float32 {
+	if a < b {
+		return a
+	} // if
+	return b
+}
+
+func maxFloat32(a, b float32) float32 {
+	if a > b {
+		return a
+	} // if
+	return b
+}
 /*
-EditDistanceStr calculates the edit-distance between two strings. Input strings must be UTF-8 encoded.
-
-The time complexity is O(mn) where m and n are lengths of a and b, and space complexity is O(n).
+EditDistanceStr calculates the standard edit-distance between two strings.
+The time complexity is O(mn) where m and n are lengths of s and t, and space complexity is O(n).
 */
-func EditDistanceStr(a, b string) int {
-	f := make([]int, utf8.RuneCountInString(b)+1)
 
-	for j := range f {
-		f[j] = j
+func EditDistanceStr(s, t string) int {
+	ls, lt := len(s), len(t)
+	lm := maxInt(ls, lt)
+
+	M0, M1 := make([]int, lm+1), make([]int, lm+1)
+
+	//Initialize the first row in ED matrix
+	for j := 0; j <= lt; j++ {
+		M0[j] = j
 	} // for j
 
-	for _, ca := range a {
-		j := 1
-		fj1 := f[0] // fj1 is the value of f[j - 1] in last iteration
-		f[0]++
-		for _, cb := range b {
-			mn := min(f[j]+1, f[j-1]+1) // delete & insert
-			if cb != ca {
-				mn = min(mn, fj1+1) // change
+	//Compute values in all rows in ED matrix
+	for i := 1; i <= ls; i++ {
+		//Initialize the first cell in row i in ED matrix
+		M1[0] = i
+
+		//Compute values in all cell in row i in ED matrix
+		for j := 1; j <= lt; j++ {
+			mn := minInt(M0[j] + 1, M1[j-1] + 1) // deletion & insertion
+			if s[i-1] == t[j-1] {
+				mn = minInt(mn, M0[j-1])         // match
 			} else {
-				mn = min(mn, fj1) // matched
-			} // else
+				mn = minInt(mn, M0[j-1] + 1)     // substitution (mismatch)
+			}
+			M1[j] = mn // min of 3 operations
+		} // for j
 
-			fj1, f[j] = f[j], mn // save f[j] to fj1(j is about to increase), update f[j] to mn
-			j++
-		} // for cb
-	} // for ca
+        //Early break if ED exceeds given threshold
+        flag := true
+        for k := 0; k <= lt; k++ {
+            if M1[k] <= Dist_Threshold {
+                flag = false
+                break
+            }
+        }
+        if flag {
+        	return Dist_Threshold + 1;
+        }
 
-	return f[len(f)-1]
+		//Swap M0 and M1
+		M := make([]int, lm+1)
+		copy(M, M0)
+		copy(M0, M1)
+		copy(M1, M)
+
+	} // for i
+
+	return M0[lt]
 }
-
-// Constants of operations
-const (
-	opDEL byte = iota
-	opINS
-	opCHANGE
-	opMATCH
-)
 
 /*
-EditDistanceStrFull calculates the edit-distance and longest-common-string between two strings. Input strings must be UTF-8 encoded.
-
-The time and space complexity are all O(mn) where m and n are lengths of a and b.
-
-NOTE if detailed matching information is not necessary, call String instead because it needs much less memories.
+Cost functions for computing edit-distance for multi-genomes.
 */
-func EditDistanceStrFull(a, b string) (dist int, lcs string) {
-	la, lb := utf8.RuneCountInString(a), utf8.RuneCountInString(b)
-	f := make([]int, lb+1)
-	ops := make([]byte, la*lb)
 
-	for j := range f {
-		f[j] = j
-	} // for j
-
-	p := 0 // the index to ops
-
-	for _, ca := range a {
-		j := 1
-		fj1 := f[0] // fj1 is the value of f[j - 1] in last iteration
-		f[0]++
-		for _, cb := range b {
-			mn, op := f[j]+1, opDEL
-			if f[j-1]+1 < mn {
-				mn, op = f[j-1]+1, opINS
-			} // if
-
-			if cb != ca {
-				if fj1+1 < mn {
-					mn, op = fj1+1, opCHANGE
-				} // if
-			} else {
-				mn, op = fj1, opMATCH
-			} // else
-
-			fj1, f[j], ops[p] = f[j], mn, op // save f[j] to fj1(j is about to increase), update f[j] to mn
-			p++
-			j++
-		} // for cb
-	} // for ca
-
-	// Calculate longest-common-string
-	lcsi := make([]int, 0, la)
-	for i, j := la, lb; i > 0 || j > 0; {
-		var op byte
-		switch {
-		case i == 0:
-			op = opINS
-		case j == 0:
-			op = opDEL
-		default:
-			op = ops[(i-1)*lb+j-1]
-		} // switch
-
-		switch op {
-		case opINS:
-			j--
-		case opDEL:
-			i--
-		case opCHANGE, opMATCH:
-			i--
-			j--
-			if op == opMATCH {
-				lcsi = append(lcsi, i)
-			} // if
-		} // switch
-	} // for i, j
-
-	if i, l := 0, len(lcsi)-1; l > 0 {
-		for _, ca := range a {
-			if i == lcsi[l] {
-				lcs = lcs + string(ca)
-				l--
-				if l < 0 {
-					break
-				} // if
-			} // if
-			i++
-		} // for ca
-	} // if
-
-	return f[len(f)-1], lcs
-}
-
-// Ternary returns vT if cond equals true, or vF otherwise.
-func Ternary(cond bool, vT, vF int) int {
-	if cond {
-		return vT
-	} // if
-
-	return vF
-}
-
-// ConstCost returns a const-function whose return value is the specified cost. This is useful for EditDistanceF and EditDistanceFFull if the del/ins cost is constant to positions.
-func ConstCost(cost int) func(int) int {
-	return func(int) int {
-		return cost
+func CostOfSub(s, t byte, pos int) float32 {
+	if std_to_iupac[s][t] {
+		return 0
+	} else {
+		_, ok := snp_pos[pos]
+		if ok {
+			return snp_sub_cost
+		}
+		return non_snp_sub_cost
 	}
 }
 
-/*
-Base is a helper type which defines LenA, LenB, CostOfDel and CostOfIns methods.
-*/
-type Base struct {
-	LA, LB, Cost int
+func CostOfIns(t byte, pos int) float32 {
+	_, ok := snp_pos[pos]
+	if ok {
+		return 1
+	} else {
+		return 1 //infty
+	}
 }
 
-// Interface.LenA
-func (b *Base) LenA() int {
-	return b.LA
-}
-
-// Interface.LenB
-func (b *Base) LenB() int {
-	return b.LB
-}
-
-// Interface.CostOfDel
-func (b *Base) CostOfDel(iA int) int {
-	return b.Cost
-}
-
-// Interface.CostOfIns
-func (b *Base) CostOfIns(iB int) int {
-	return b.Cost
+func CostOfDel(s byte) float32 {
+	return 1//infty
 }
 
 /*
-Interface defines a pair of lists and the cost for each operation.
-*/
-type Interface interface {
-	// LenA returns the lenght of the source list
-	LenA() int
-
-	// LenB returns the lenght of the destination list
-	LenB() int
-
-	// CostOfChange returns the change cost from an item in the source list at iA to an item in the destination list at iB
-	CostOfChange(iA, iB int) int
-
-	// CostOfDel returns the cost of deleting an item in the source list at iA
-	CostOfDel(iA int) int
-
-	// CostOfIns returns the cost of inserting an item in the destination list at iB
-	CostOfIns(iB int) int
-}
-
-/*
-EditDistance returns the edit-distance defined by Interface.
-
+EditDistance returns the edit-distance between reads and (a part of) multi-genomes.
 The time complexity is O(mn) where m and n are lengths of a and b, and space complexity is O(n).
 */
-func EditDistance(in Interface) int {
-	la, lb := in.LenA(), in.LenB()
+func EditDistance(s, t []byte, pos int) float32 {
+	ls, lt := len(s), len(t)
+	lm := maxInt(ls, lt)
 
-	f := make([]int, lb+1)
+	M0, M1 := make([]float32, lm+1), make([]float32, lm+1)
 
-	for j := 1; j <= lb; j++ {
-		f[j] = f[j-1] + in.CostOfIns(j-1)
+	//Initialize the first row in ED matrix
+	M0[0] = 0
+	for j := 1; j <= lt; j++ {
+		M0[j] = M0[j-1] + CostOfIns(t[j-1], pos)
 	} // for j
 
-	for i := 0; i < la; i++ {
-		fj1 := f[0] // fj1 is the value of f[j - 1] in last iteration
-		f[0] += in.CostOfDel(i)
-		for j := 1; j <= lb; j++ {
-			mn := min(f[j]+in.CostOfDel(i), f[j-1]+in.CostOfIns(j-1)) // delete & insert
-			mn = min(mn, fj1+in.CostOfChange(i, j-1))                 // change/matched
+	//Compute values in all rows in ED matrix
+	for i := 1; i <= ls; i++ {
+		//Initialize the first cell in row i in ED matrix
+		M1[0] = M0[0]+ CostOfDel(s[i-1])
 
-			fj1, f[j] = f[j], mn // save f[j] to fj1(j is about to increase), update f[j] to mn
+		//Compute values in all cell in row i in ED matrix
+		for j := 1; j <= lt; j++ {
+			mn := minFloat32(M0[j] + CostOfDel(s[i-1]), M1[j-1] + CostOfIns(t[j-1], pos)) // deletion & insertion
+			mn = minFloat32(mn, M0[j-1] + CostOfSub(s[i-1], t[j-1], pos))                 // match/mismatch
+			M1[j] = mn // min of 3 operations
 		} // for j
+
+        //Early break if ED exceeds given threshold
+        flag := true
+        for k := 0; k <= lt; k++ {
+            if M1[k] <= float32(Dist_Threshold) {
+                flag = false
+                break
+            }
+        }
+        if flag {
+        	return float32(Dist_Threshold + 1);
+        }
+
+		//Swap M0 and M1
+		M := make([]float32, lm+1)
+		copy(M, M0)
+		copy(M0, M1)
+		copy(M1, M)
+
 	} // for i
 
-	return f[lb]
-}
-
-func matchingFromOps(la, lb int, ops []byte) (matA, matB []int) {
-	matA, matB = make([]int, la), make([]int, lb)
-	for i, j := la, lb; i > 0 || j > 0; {
-		var op byte
-		switch {
-		case i == 0:
-			op = opINS
-		case j == 0:
-			op = opDEL
-		default:
-			op = ops[(i-1)*lb+j-1]
-		} // switch
-
-		switch op {
-		case opINS:
-			j--
-			matB[j] = -1
-		case opDEL:
-			i--
-			matA[i] = -1
-		case opCHANGE:
-			i--
-			j--
-			matA[i], matB[j] = j, i
-		} // switch
-	} // for i, j
-
-	return matA, matB
+	return M0[lt]
 }
 
 /*
 EditDistanceFull returns the edit-distance and corresponding match indexes defined by Interface.
-Each element in matA and matB is the index in the other list, if it is equal to or greater than zero; or -1 meaning a deleting or inserting in matA or matB, respectively.
+Each element in matA and matB is the index in the other list, if it is equal to or greater than zero;
+ or -1 meaning a deleting or inserting in matA or matB, respectively.
 
 The time and space complexity are all O(mn) where m and n are lengths of a and b.
 
 NOTE if detailed matching information is not necessary, call EditDistance instead because it needs much less memories.
 */
+/*
 func EditDistanceFull(in Interface) (dist int, matA, matB []int) {
 	la, lb := in.LenA(), in.LenB()
 
@@ -347,78 +275,4 @@ func EditDistanceFull(in Interface) (dist int, matA, matB []int) {
 
 	return f[lb], matA, matB
 }
-
-/*
-EditDistanceFunc returns the edit-distance defined by parameters and functions.
-
-The time complexity is O(mn) where m and n are lengths of a and b, and space complexity is O(n).
 */
-func EditDistanceFunc(lenA, lenB int, costOfChange func(iA, iB int) int, costOfDel func(iA int) int, costOfIns func(iB int) int) int {
-	la, lb := lenA, lenB
-
-	f := make([]int, lb+1)
-
-	for j := 1; j <= lb; j++ {
-		f[j] = f[j-1] + costOfIns(j-1)
-	} // for j
-
-	for i := 0; i < la; i++ {
-		fj1 := f[0] // fj1 is the value of f[j - 1] in last iteration
-		f[0] += costOfDel(i)
-		for j := 1; j <= lb; j++ {
-			mn := min(f[j]+costOfDel(i), f[j-1]+costOfIns(j-1)) // delete & insert
-			mn = min(mn, fj1+costOfChange(i, j-1))              // change/matched
-
-			fj1, f[j] = f[j], mn // save f[j] to fj1(j is about to increase), update f[j] to mn
-		} // for j
-	} // for i
-
-	return f[lb]
-}
-
-/*
-EditDistanceFuncFull returns the edit-distance and corresponding match indexes defined by parameters and functions.
-Each element in matA and matB is the index in the other list, if it is equal to or greater than zero; or -1 meaning a deleting or inserting in matA or matB, respectively.
-
-The time and space complexity are all O(mn) where m and n are lengths of a and b.
-
-NOTE if detailed matching information is not necessary, call EditDistance instead because it needs much less memories.
-*/
-func EditDistanceFuncFull(lenA, lenB int, costOfChange func(iA, iB int) int, costOfDel func(iA int) int, costOfIns func(iB int) int) (dist int, matA, matB []int) {
-	la, lb := lenA, lenB
-
-	f := make([]int, lb+1)
-	ops := make([]byte, la*lb)
-
-	for j := 1; j <= lb; j++ {
-		f[j] = f[j-1] + costOfIns(j-1)
-	} // for j
-
-	// Matching with dynamic programming
-	p := 0
-	for i := 0; i < la; i++ {
-		fj1 := f[0] // fj1 is the value of f[j - 1] in last iteration
-		f[0] += costOfDel(i)
-		for j := 1; j <= lb; j++ {
-			mn, op := f[j]+costOfDel(i), opDEL // delete
-
-			if v := f[j-1] + costOfIns(j-1); v < mn {
-				// insert
-				mn, op = v, opINS
-			} // if
-
-			// change/matched
-			if v := fj1 + costOfChange(i, j-1); v < mn {
-				// insert
-				mn, op = v, opCHANGE
-			} // if
-
-			fj1, f[j], ops[p] = f[j], mn, op // save f[j] to fj1(j is about to increase), update f[j] to mn
-			p++
-		} // for j
-	} // for i
-	// Reversely find the match info
-	matA, matB = matchingFromOps(la, lb, ops)
-
-	return f[lb], matA, matB
-}
--- distance_test.go
+++ distance_test.go
@@ -21,30 +21,18 @@ func __(name string) {
 	fmt.Println("== END", name, "===")
 }
 
-type unitStr struct {
-	Base
-	a, b []byte
-}
-
-func (us *unitStr) CostOfChange(iA, iB int) int {
-	if us.a[iA] == us.b[iB] {
-		return 0
-	} // if
-
-	return 1
-}
-
 func TestEditDistanceStr(t *testing.T) {
 	defer __(o_())
 
 	cases := [][]interface{}{
-		{"abcd", "bcde", 2},
+		{"", "", 0},
 		{"abcde", "", 5},
 		{"", "abcde", 5},
-		{"", "", 0},
+		{"abcd", "bcde", 2},
 		{"abcde", "abcde", 0},
 		{"abcde", "dabce", 2},
-		{"abcde", "abfde", 1}}
+		{"abcde", "abfde", 1},
+	}
 
 	for _, cs := range cases {
 		d := EditDistanceStr(cs[0].(string), cs[1].(string))
@@ -53,106 +41,56 @@ func TestEditDistanceStr(t *testing.T) {
 		} // if
 	} // for case
 
-	for _, cs := range cases {
-		a, b := []byte(cs[0].(string)), []byte(cs[1].(string))
-		d := EditDistance(&unitStr{Base{len(a), len(b), 1}, a, b})
-		if d != cs[2].(int) {
-			t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2], d)
-		} // if
-	} // for case
-}
-
-func TestEditDistanceStrFull(t *testing.T) {
-	defer __(o_())
-
-	cases := [][]interface{}{
-		{"abcd", "bcde", 2, "bcd"},
-		{"abcde", "", 5, ""},
-		{"", "abcde", 5, ""},
-		{"", "", 0, ""},
-		{"abcde", "abcde", 0, "abcde"},
-		{"abcde", "dabce", 2, "abce"},
-		{"abcde", "abfde", 1, "abde"}}
-
-	for _, cs := range cases {
-		d, lcs := EditDistanceStrFull(cs[0].(string), cs[1].(string))
-		if d != cs[2].(int) {
-			t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2], d)
-		} // if
-		if lcs != cs[3].(string) {
-			t.Errorf("Longest-common-string between %s and %s is expected to be %s, but %s got!", cs[0], cs[1], cs[3], lcs)
-		} // if
-	} // for case
-}
-
-func ExampleString() {
-	fmt.Println(EditDistanceStr("abcde", "bfdeg"))
-
-	fmt.Println(EditDistanceStrFull("abcde", "bfdeg"))
-	/* Output:
-	3
-3 bde
-	*/
-}
-
-type stringInterface struct {
-	a, b []byte
-}
-
-func (in *stringInterface) LenA() int {
-	return len(in.a)
-}
-
-func (in *stringInterface) LenB() int {
-	return len(in.b)
-}
-
-func (in *stringInterface) CostOfChange(iA, iB int) int {
-	if base_cmp(in.a[iA], in.b[iB]) {
-		return 0
-	} // if
-
-	return 1
-}
-
-func (in *stringInterface) CostOfDel(iA int) int {
-	return 1
-}
-
-func (in *stringInterface) CostOfIns(iB int) int {
-	return 1
 }
 
 func TestEditDistance(t *testing.T) {
 	defer __(o_())
 
 	cases := [][]interface{}{
+		{"", "", 0},
 		{"ACGTACGT", "", 8},
 		{"", "ACGTACGT", 8},
 		{"ACGT", "ACGT", 0},
-		{"ACGT", "CGTA", 2},
-		{"ACGAT", "TACGT", 2},
-		{"GTCAC", "GTTAC", 1},
-		{"TGCGAATGCTGACATAAGTAACGATAAAGCGGGTGAAAAGCCCGCTCGCCGGAAGACCAAGGGTTCCTGTCCAACGTTAATCGGGGCAGGGGGAGTCGAC",
-		 "TCGACTCACCCTGCCCCGATTAACGTTGGACAGGAACCCTTGGTCTTCCGGCGAGCGGGCTTTTCACCCGCTTTATCGTTACTTATGTCAGCATTCGCAC", 52},
-		{"AGGCCTCATTACCAGATTTGCGGTAGCCTTTTATCCAGTTTTTAACCGTGTTGTGGCTGGGAATGTTAAACCGGGCAGCTACCCGGGGTAGAGACTCAGA",
-		 "CTGAGTCTCTACCCCGGGTAGCTGCCCGGTTTAACATTCCCAGCCACAACACGGTTAAAAACTGGATAAAAGGCTACCGCAAATCTGGTAATGAGGCCTT", 56},
-		{"AGCBBHTYHHHWDWD", "", 15},
-		{"", "AGCBBHTYHHHWDWD", 15},
-		{"AGCBBHTYHHHWDWD", "AGCCTTCAATATT", 2},
-		{"AGCBBHTYHHHWDWD", "AGCCATCAATATT", 2},
-		{"AGCBBHTYHHHWDWD", "AGCCTTCAATATC", 3},
-		{"AGCBBHTYHHHWDWD", "AGCCGTCAATATT", 2},
-		{"AGCBBHTYHHHWDWD", "AGCCATCATATT", 3},
-		{"TGCGAATGCTGRCATAAGTAACGABAAAGCGGGTGAAAAGCCCGCTCGCCGGAAGACHAAGGGTTCCTGTCCAACGTTAATCGGGGCAGGGGGAGTCGAV",
-		 "TCGACTCACCCTGCCCCGATTAACGTTGGACAGGAACCCTTGGTCTTCCGGCGAGCGGGCTTTTCACCCGCTTTATCGTTACTTATGTCAGCATTCGCAC", 52},
-		{"AGGCCTCRTTACCAGATTTKCGGTAGCCTTTTATCCABTTTTTAACCGTGTTGTGGCTGGGHATGTTAAACCGGGCAGCTACCCGGGGTAGAGACTCAGA",
-		 "CTGAGTCTCTACCCCGGGTAGCTGCCCGGTTTAACATTCCCAGCCACAACACGGTTAAAAACTGGATAAAAGGCTACCGCAAATCTGGTAATGAGGCCTT", 56},
-		{"", "", 0}}
+		{"CGTA", "ACGT", 2},
+		{"TACGT", "ACGAT", 2},
+		{"GTTAC", "GTCAC", 1},
+		{"TCGACTCACCCTGCCCCGATTAACGTTGGACAGGAACCCTTGGTCTTCCGGCGAGCGGGCTTTTCACCCGCTTTATCGTTACTTATGTCAGCATTCGCAC",
+		 "TGCGAATGCTGACATAAGTAACGATAAAGCGGGTGAAAAGCCCGCTCGCCGGAAGACCAAGGGTTCCTGTCCAACGTTAATCGGGGCAGGGGGAGTCGAC", 52},
+		{"CTGAGTCTCTACCCCGGGTAGCTGCCCGGTTTAACATTCCCAGCCACAACACGGTTAAAAACTGGATAAAAGGCTACCGCAAATCTGGTAATGAGGCCTT",
+		 "AGGCCTCATTACCAGATTTGCGGTAGCCTTTTATCCAGTTTTTAACCGTGTTGTGGCTGGGAATGTTAAACCGGGCAGCTACCCGGGGTAGAGACTCAGA", 56},
+	}
 
 	for _, cs := range cases {
-		d := EditDistance(&stringInterface{[]byte(cs[0].(string)), []byte(cs[1].(string))})
-		if d != cs[2].(int) {
+		d := EditDistance([]byte(cs[0].(string)), []byte(cs[1].(string)), 0)
+		if d != float32(cs[2].(int)) {
+			t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2].(int), d)
+		} // if
+	} // for case
+}
+
+func TestEditDistanceIUPAC(t *testing.T) {
+	defer __(o_())
+
+	cases := [][]interface{}{
+		{"", "", 0},
+		{"AGCBBHTYHHHWDWD", "", 15},
+		{"", "AGCBBHTYHHHWDWD", 15},
+
+		{"AGCCTTCAATATT", "AGCBBHTYHHHWDWD", 2},
+		{"AGCCATCAATATT", "AGCBBHTYHHHWDWD", 2},
+		{"AGCCTTCAATATC", "AGCBBHTYHHHWDWD", 3},
+		{"AGCCGTCAATATT", "AGCBBHTYHHHWDWD", 2},
+		{"AGCCATCATATT" , "AGCBBHTYHHHWDWD", 3},
+
+		{"TCGACTCACCCTGCCCCGATTAACGTTGGACAGGAACCCTTGGTCTTCCGGCGAGCGGGCTTTTCACCCGCTTTATCGTTACTTATGTCAGCATTCGCAC",
+		 "TGCGAATGCTGRCATAAGTAACGABAAAGCGGGTGAAAAGCCCGCTCGCCGGAAGACHAAGGGTTCCTGTCCAACGTTAATCGGGGCAGGGGGAGTCGAV", 52},
+
+		{"CTGAGTCTCTACCCCGGGTAGCTGCCCGGTTTAACATTCCCAGCCACAACACGGTTAAAAACTGGATAAAAGGCTACCGCAAATCTGGTAATGAGGCCTT",
+		 "AGGCCTCRTTACCAGATTTKCGGTAGCCTTTTATCCABTTTTTAACCGTGTTGTGGCTGGGHATGTTAAACCGGGCAGCTACCCGGGGTAGAGACTCAGA", 56},
+	}
+	for _, cs := range cases {
+		d := EditDistance([]byte(cs[0].(string)), []byte(cs[1].(string)), 0)
+		if d != float32(cs[2].(int)) {
 			t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2].(int), d)
 		} // if
 	} // for case
@@ -183,90 +121,5 @@ func TestEditDistanceFull(t *testing.T) {
 			t.Errorf("matB for matchting between %s and %s is expected to be %v, but %v got!", cs[0], cs[1], cs[4], matB)
 		} // if
 	} // for case
-}
-*/
-/*
-func TestEditDistanceFunc(t *testing.T) {
-	defer __(o_())
-
-	cases := [][]interface{}{
-		{"abcd", "bcde", 2},
-		{"abcde", "", 5},
-		{"", "abcde", 5},
-		{"", "", 0},
-		{"abcde", "abcde", 0},
-		{"abcde", "dabce", 2},
-		{"abcde", "abfde", 1}}
-
-	for _, cs := range cases {
-		a, b := []rune(cs[0].(string)), []rune(cs[1].(string))
-		d := EditDistanceFunc(len(a), len(b),
-			func(iA, iB int) int {
-				if a[iA] == b[iB] {
-					return 0
-				} // if
-
-				return 100
-			},
-			func(iA int) int {
-				return 100 + iA
-			},
-			func(iB int) int {
-				return 110 + iB
-			})
-		if d != cs[2].(int) {
-			t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2].(int), d)
-		} // if
-	} // for case
-}
-
-func TestEditDistanceFuncFull(t *testing.T) {
-	defer __(o_())
-
-	cases := [][]interface{}{
-		{"abcd", "bcde", 2, []int{-1, 0, 1, 2}, []int{1, 2, 3, -1}},
-		{"abcde", "", 5, []int{-1, -1, -1, -1, -1}, []int{}},
-		{"", "abcde", 5, []int{}, []int{-1, -1, -1, -1, -1}},
-		{"", "", 0, []int{}, []int{}},
-		{"abcde", "abcde", 0, []int{0, 1, 2, 3, 4}, []int{0, 1, 2, 3, 4}},
-		{"abcde", "dabce", 2, []int{1, 2, 3, -1, 4}, []int{-1, 0, 1, 2, 4}},
-		{"abcde", "abfde", 1, []int{0, 1, 2, 3, 4}, []int{0, 1, 2, 3, 4}}}
-
-	for _, cs := range cases {
-		a, b := []rune(cs[0].(string)), []rune(cs[1].(string))
-		d, matA, matB := EditDistanceFuncFull(len(a), len(b),
-			func(iA, iB int) int {
-				if a[iA] == b[iB] {
-					return 0
-				} // if
-
-				return 100
-			},
-			func(iA int) int {
-				return 100 + iA
-			},
-			func(iB int) int {
-				return 110 + iB
-			})
-		if d != cs[2].(int) {
-			t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2].(int), d)
-		} // if
-		if fmt.Sprint(matA) != fmt.Sprint(cs[3]) {
-			t.Errorf("matA for matchting between %s and %s is expected to be %v, but %v got!", cs[0], cs[1], cs[3], matA)
-		} // if
-		if fmt.Sprint(matB) != fmt.Sprint(cs[4]) {
-			t.Errorf("matB for matchting between %s and %s is expected to be %v, but %v got!", cs[0], cs[1], cs[4], matB)
-		} // if
-	} // for case
-}
-
-func ExampleEditDistanceFunc() {
-	a, b := "abcd", "bcde"
-	d := EditDistanceFunc(len(a), len(b), func(iA, iB int) int {
-		return Ternary(a[iA] == b[iB], 0, 1)
-	}, ConstCost(1), ConstCost(1))
-	fmt.Println(a, b, d)
-	// Output:
-	// abcd bcde 2
 }
 */
