c6ba0b0424e128ee94b000fc07bcb2b2fc54f7b3
Nam Sy Vo <nsvo1@memphis.edu>
2013-12-18 09:39:03 -0600

    working versions

--- distance.go
+++ distance.go
@@ -1,18 +1,21 @@
-/*
-Distance package provides some functions for Hamming and edit-distance calculation.
+//-------------------------------------------------------------------------------------------------
+// Copyright 2013 Nam S. Vo
 
-EditDistanceStr calculates the standard edit-distance.
+// Distance package provides some functions for distance between reads and "starred" multi-genomes.
 
-EditDistance calculates the edit-distance for multi-genomes,
- and EditDistanceFull returns extra matching INFomation.
-*/
+// DistanceMulti calculates the distance between reads and "starred" multi-genomes.
+//-------------------------------------------------------------------------------------------------
 
 package distance
 
 import (
 	"math"
-    "fmt"
+	"fmt"
 )
+
+//-------------------------------------------------------------------------------------------------
+//Utility functions: max, min
+//-------------------------------------------------------------------------------------------------
 
 func minInt(a, b int) int {
 	if a < b {
@@ -28,302 +31,267 @@ func maxInt(a, b int) int {
 	return b
 }
 
-func mINFloat32(a, b float32) float32 {
+func min3Int(a, b, c int) int {
 	if a < b {
-		return a
-	} // if
-	return b
+		if a < c {
+			return a
+		}
+	} else {
+		if b < c {
+			return b
+		}
+	}
+	return c
 }
 
-func maxFloat32(a, b float32) float32 {
-	if a > b {
-		return a
-	} // if
-	return b
+func min3Float32(a, b, c float32) float32 {
+	if a < b {
+		if a < c {
+			return a
+		}
+	} else {
+		if b < c {
+			return b
+		}
+	}
+	return c
 }
 
-/*
-EditDistanceStr calculates the standard edit-distance between two strings.
-Memory-improved version.
-The time complexity is O(mn) where m and n are lengths of s and t, and space complexity is O(n).
-*/
-
-func EditDistanceStr(s, t string) int {
-	ls, lt := len(s), len(t)
-	lm := maxInt(ls, lt)
-
-	M0, M1 := make([]int, lm+1), make([]int, lm+1)
-
-	//Initialize the first row in ED matrix
-	for j := 0; j <= lt; j++ {
-		M0[j] = j
-	} // for j
-	//Compute values in all rows in ED matrix
-	for i := 1; i <= ls; i++ {
-		//Initialize the first cell in row i in ED matrix
-		M1[0] = i
-
-		//Compute values in all cell in row i in ED matrix
-		for j := 1; j <= lt; j++ {
-			mn := minInt(M0[j] + 1, M1[j-1] + 1) // deletion & insertion
-			if s[i-1] == t[j-1] {
-				mn = minInt(mn, M0[j-1])         // match
-			} else {
-				mn = minInt(mn, M0[j-1] + 1)     // substitution (mismatch)
-			}
-			M1[j] = mn // min of 3 operations
-		} // for j
-
-        //Early break if ED exceeds given threshold
-        flag := true
-        for k := 0; k <= lt; k++ {
-            if M1[k] <= DIST_THRES {
-                flag = false
-                break
-            }
+// Minimum of a slice of int arguments
+func MinIntS(v []int) (m int) {
+        if len(v) > 0 {
+                m = v[0]
         }
-        if flag {
-        	return DIST_THRES + 1;
+        for i := 1; i < len(v); i++ {
+                if v[i] < m {
+                        m = v[i]
+                }
         }
-
-		//Swap M0 and M1
-		M := make([]int, lm+1)
-		copy(M, M0)
-		copy(M0, M1)
-		copy(M1, M)
-
-	} // for i
-
-	return M0[lt]
+        return
 }
 
-/*
-Map of standard bases (A, C, G, T) and extended IUPAC bases.
-A standard base is mapped into any extended IUPAC base that it belongs to.
-An extended IUPAC base is an IUPAC base or a gap.
-*/
-
-var std_to_iupac = map[byte]map[byte]bool {
-    'A': map[byte]bool{
-	    'A': true, 'R': true, 'M': true, 'W': true, 'D': true, 'H': true, 'V': true, 'N': true,
-    	'a': true, 'r': true, 'm': true, 'w': true, 'd': true, 'h': true, 'v': true, 'n': true,
-    	},
-    'C': map[byte]bool{
-    	'C': true, 'Y': true, 'M': true, 'S': true, 'B': true, 'H': true, 'V': true, 'N': true,
-    	'c': true, 'y': true, 'm': true, 's': true, 'b': true, 'h': true, 'v': true, 'n': true,
-    	},
-    'G': map[byte]bool{
-    	'G': true, 'R': true, 'K': true, 'S': true, 'B': true, 'D': true, 'V': true, 'N': true,
-    	'g': true, 'r': true, 'k': true, 's': true, 'b': true, 'd': true, 'v': true, 'n': true,
-		},
-    'T': map[byte]bool{
-	    'T': true, 'Y': true, 'K': true, 'W': true, 'B': true, 'D': true, 'H': true, 'N': true,
-    	't': true, 'y': true, 'k': true, 'w': true, 'b': true, 'd': true, 'h': true, 'n': true,
-		},
+// Minimum of a variable number of int arguments
+func MinIntV(v1 int, vn ...int) (m int) {
+        m = v1
+        for i := 0; i < len(vn); i++ {
+                if vn[i] < m {
+                        m = vn[i]
+                }
+        }
+        return
 }
 
-//Print Std-IUPAC map
-func PrintMaps(){
-	fmt.Println(std_to_iupac)
-}
+//-------------------------------------------------------------------------------------------------
+// Functions for distance between reads and multi-genomes.
+//-------------------------------------------------------------------------------------------------
 
-/*
-Map of SNP region and multi-genomes.
-A position on SNP region is mapped into an actual position on the multi-genome.
-*/
-var snp_pos = map[int]int{
-	3:100,
-	4:100,
-	5:100,
-	6:100,
-	7:100,
-	8:100,
-	9:100,
-	10:100,
-	11:100,
-	12:100,
-	13:100,
-	14:100,
-}
+//-------------------------------------------------------------------------------------------------
+// Constants and global variables.
+//-------------------------------------------------------------------------------------------------
 
-//Const for computing cost for edit distance
-var r_e float32 = 0.1
-var r_m float32 = 0.01
-var r_x float32 = 0.05
+// Cost for substitution with error sequencing
+var ERR_COST float32 = 1
 
-var SNP_SUB_COST float32 = float32(math.Log10(float64(1/r_e)) + math.Log10(float64(1/r_m))) // = 3
-var N_SNP_SUB_COST float32 = float32(math.Log10(float64(1/r_e))) // = 1
-
-var INF float32 = float32(math.MaxInt32)
+// Distance threshold for early break
 var DIST_THRES int = 1000
 
+// SNP profile
+var SNP_PROFILE map[int][][]byte
 
-//Cost functions for computing edit-distance for multi-genomes.
+// SNP profile
+var SAME_LEN_SNP map[int]int
 
-// Cost for aligning (x_i, -)
-var CostOfDel = INF
+// Approx value for Infinity
+var INF int = math.MaxInt32
 
-// Cost for aligning (-, y_j)
-// Go should called this as an inline function
-func CostOfIns(t byte) float32 {
-	//If t is lowercase (that is, an IUPAC base or a gap)
-	if 97 <= t && t <= 122 {
-		return 1
-	} else {
-		return INF
-	}
-}
+//-------------------------------------------------------------------------------------------------
+// Cost functions for computing distance between reads and multi-genomes.
+//-------------------------------------------------------------------------------------------------
 
-// Cost for aligning (x_i, y_j)
-func CostOfSub(s, t byte, pos int) float32 {
-	if s == t {
-		return 0
-	}
-	//Check for SNP position
-	_, is_snp := snp_pos[pos]
-	if !is_snp {
-		return N_SNP_SUB_COST
-	} else {
-		//Check for match between a standard base and an extended IUPAC base
-		_, matched := std_to_iupac[s][t]
-		if matched {
-			return 0
+// Cost for "SNP match"
+func Cost(s, t []byte) int {
+	mismatch := 0
+	for i:= 0; i < len(s); i++ {
+		if s[i] != t[i] {
+			mismatch++
 		}
-		return SNP_SUB_COST
 	}
+	return mismatch
 }
 
+//-------------------------------------------------------------------------------------------------
+// Functions for computing distance between reads and multi-genomes.
+//-------------------------------------------------------------------------------------------------
 
-/*
-HammingDistance returns the Hamming distance between reads (s) and (a part of) multi-genome (t),
-s is a read
-t is part of the SNP region
-The time complexity is O(min(m, n)) where m and n are lengths of s and t.
-*/
+// Initilize constants and global variables
+func Init(pERR_COST float32, pDIST_THRES int, pSNP_PROFILE map[int][][]byte, pSAME_LEN_SNP map[int]int) {
+	ERR_COST = pERR_COST
+	DIST_THRES = pDIST_THRES
+	SNP_PROFILE = pSNP_PROFILE
+	SAME_LEN_SNP = pSAME_LEN_SNP
+}
 
-func HammingDistance(s, t []byte) float32 {
-	lm := minInt(len(s), len(t))
-	var dis float32 = 0;
-	//Compute values in all rows in ED matrix
-	for i := 0; i < lm; i++ {
-        dis = dis + CostOfSub(s[i], t[i], i)
+//-------------------------------------------------------------------------------------------------
+// Calculate the distance between reads and multi-genomes.
+// The reads include standard bases, the multi-genomes include standard bases and "*" characters.
+// 	s is a read.
+// 	t is part of a multi-genome.
+//-------------------------------------------------------------------------------------------------
+func DistanceMulti(s, t []byte, pos int) (float32, int, int, int, [][]int) {
 
-        //Early break if ED exceeds given threshold
-        if dis > float32(DIST_THRES) {
-            break
-        }
+	//fmt.Println(string(s), string(t))
+    m, n := len(s), len(t)
+    var i, j, k int
+
+    //"Hamming distance" part
+    var cost int
+    var d, min_d int
+    var snp_len int
+    var snp_values [][]byte
+    var is_snp, is_same_len_snp bool
+
+    d = 0
+    for m > 0 && n > 0 {
+		snp_values, is_snp = SNP_PROFILE[pos + n - 1]
+		snp_len, is_same_len_snp = SAME_LEN_SNP[pos + n - 1]
+    	if !is_snp {
+        	if s[m-1] != t[n-1] {
+        		d++
+        	}
+    		m--
+    		n--
+    	} else if is_same_len_snp {
+			min_d = INF
+    		for i = 0; i < len(snp_values); i++ {
+    			cost = Cost(s[m - snp_len: m], snp_values[i])
+    			if min_d > cost {
+    				min_d = cost
+    			}
+    		}
+    		d += min_d
+    		m -= snp_len
+    		n--
+    	} else {
+    		break
+    	}
     }
-	return dis
-}
+    //fmt.Println(d, m, n)
 
-/*
-EditDistance returns the edit-distance between reads (s) and (a part of) multi-genome (t),
-s is a read
-t is part of the SNP region
-using the Levenshtein algorithm with a memory-improved version using two 1-D arrays.
-The time complexity is O(mn) where m and n are lengths of s and t, and space complexity is O(m).
-*/
+    //"Edit distance" part
 
-func EditDistanceArray(s, t []byte) float32 {
-	ls, lt := len(s), len(t)
-	lm := maxInt(ls, lt)
-
-	// Two 1-D array storing dynamic programing values
-	M0, M1 := make([]float32, lm+1), make([]float32, lm+1)
-
-	//Initialize the first row in ED matrix
-	M0[0] = 0
-	for j := 1; j <= lt; j++ {
-		M0[j] = M0[j-1] + CostOfIns(t[j-1])
-	} // for j
-
-	//Compute values in all rows in ED matrix
-	for i := 1; i <= ls; i++ {
-		//Initialize the first cell in row i in ED matrix
-		M1[0] = M0[0]+ CostOfDel
-		//Compute values in all cell in row i in ED matrix
-		for j := 1; j <= lt; j++ {
-            insCost := M1[j-1] + CostOfIns(t[j-1])
-            delCost := M0[j] + CostOfDel
-            matchSubCost := M0[j-1] + CostOfSub(s[i-1], t[j-1], j-1)
-			M1[j] = mINFloat32(delCost, mINFloat32(matchSubCost, insCost))
-		} // for j
-
-        //Early break if ED exceeds given threshold
-        flag := true
-        for k := 0; k <= lt; k++ {
-            if M1[k] <= float32(DIST_THRES) {
-                flag = false
-                break
-            }
-        }
-        if flag {
-        	return float32(DIST_THRES + 1);
-        }
-
-		//Swap M0 and M1
-		M := make([]float32, lm+1)
-		copy(M, M0)
-		copy(M0, M1)
-		copy(M1, M)
-
-	} // for i
-
-	return M0[lt]
-}
-
-/*
-EditDistance returns the edit-distance between reads (s) and (a part of) multi-genome (t),
-s is a read
-t is part of the SNP region
-using the Levenshtein algorithm with dynamic programming table as 2-D matrix.
-The time complexity is O(mn) where m and n are lengths of a and b, and space complexity is O(mn).
-*/
-
-func EditDistanceMatrix(s, t []byte) float32 {
-    // Make a 2-D matrix: rows correspond to prefixes of source, columns to prefixes of target.
-    // Cells will contain edit distances.
-    ls, lt := len(s), len(t)
-    H := make([][]float32, ls + 1)
-
+    // Make a 2-D matrix:
+    // 	rows correspond to prefixes of source, columns to prefixes of target.
+    // 	cells contain edit distances.
+    H := make([][]int, m + 1)
+    for i = 0; i <= m; i++ {
+            H[i] = make([]int, n + 1)
+    }
     // Initialize distances (base cases, from/to empty string).
-    // Fill the left column and the top row with row/column indices.
-    for i := 0; i <= ls; i++ {
-            H[i] = make([]float32, lt + 1)
-    }
-
-	//Initialize the first row in ED matrix
+	// Initialize the first row in distance matrix
     H[0][0] = 0
-    for i := 1; i <= ls; i++ {
-            H[i][0] = H[i-1][0] + CostOfDel
+	//Initialize the first column in distance matrix
+    for i = 1; i <= m; i++ {
+		H[i][0] = INF
     }
-	//Initialize the first column in ED matrix
-    for j := 1; j <= lt; j++ {
-            H[0][j] = H[0][j-1] + CostOfIns(t[j-1])
+	//Initialize the first row in distance matrix
+    for j = 1; j <= n; j++ {
+		H[0][j] = 0
     }
-
+	//fmt.Println(H)
     // Fill in the remaining cells follow the Levenshtein algorithm.
-	//Compute values in all rows in ED matrix
-    for i := 1; i <= ls; i++ {
-		//Compute values in all cell in row i in ED matrix
-        for j := 1; j <= lt; j++ {
-            insCost := H[i][j - 1] + CostOfIns(t[j-1])
-            delCost := H[i - 1][j] + CostOfDel
-            matchSubCost := H[i - 1][j - 1] + CostOfSub(s[i-1], t[j-1], j-1)
-            H[i][j] = mINFloat32(delCost, mINFloat32(matchSubCost, insCost))
-       	}
-		//Early break if ED exceeds given threshold
+	//Compute values in all rows in distance matrix
+	var temp int
+    for i = 1; i <= m; i++ {
+		//fmt.Println("i: ", i)
+		//Compute values in all cell in row i in distance matrix
+        for j = 1; j <= n; j++ {
+			snp_values, is_snp = SNP_PROFILE[pos + j - 1]
+	    	if !is_snp {
+				if s[i-1] != t[j-1] {
+					H[i][j] = H[i - 1][j - 1] + 1
+				} else {
+					H[i][j] = H[i - 1][j - 1]
+				}
+	   		} else {
+				H[i][j] = INF
+				for k = 0; k < len(snp_values); k++ {
+					if i - len(snp_values[k]) >= 0 {
+						if snp_values[k][0] == '-' {
+	    						temp = H[i][j - 1]
+						} else {
+							temp = H[i - len(snp_values[k])][j - 1] + Cost(s[i - len(snp_values[k]) : i], snp_values[k])
+						}
+	    				if H[i][j] > temp {
+	    					H[i][j] = temp
+	    				}
+						//fmt.Println(">>> ",k, snp_values[k], string(s[i - len(snp_values[k]) : i]), string(snp_values[k]), d)
+					}
+	   			}
+			}
+			//fmt.Println("j: ", j, ": ", H[i][j], " ")
+		}
+		//fmt.Println()
+/*		//Early break if ED exceeds given threshold
 		flag := true
-		for k := 0; k <= lt; k++ {
-		    if H[i][k] <= float32(DIST_THRES) {
+		for k = 0; k <= n; k++ {
+		    if H[i][k] <= DIST_THRES - d {
 		        flag = false
 		        break
 		    }
 		}
 		if flag {
-			return float32(DIST_THRES + 1);
+			return float32(DIST_THRES + 1) * ERR_COST, d, m, n, [][]int{}
 		}
-    }
-    //LogMatrix(source, target, matrix)
-    return H[ls][lt]
+*/
+	}
+	//fmt.Println(H)
+    return float32(d + H[m][n]) * ERR_COST, d, m, n, H
+}
+
+
+//-------------------------------------------------------------------------------------------------
+// Construct an alignment of s1 and s2 as pair of indexes, traceback from dynamic programming table
+//-------------------------------------------------------------------------------------------------
+func TraceBack(s1, s2 []byte, d [][]float32, pos int) []int {
+
+	//trace back to find number of insertions / deletions / substitutions
+	var i_count, d_count, s_count int = 0, 0, 0
+
+	var i, j int = int(len(s1)), int(len(s2))
+	//fmt.Println("(", i, ", ", j, "); ")
+	for  i > 0 || j > 0 {
+
+		if i > 0 && j > 0 {
+			var diff float32 = d[i][j] - min3Float32(d[i - 1][j - 1], d[i - 1][j], d[i][j - 1]);
+		  	if diff == (d[i][j] - d[i - 1][j - 1]) {
+				if s1[i-1] == s2[j-1] { // matched, no operation
+		  			fmt.Println("(", i - 1, ", ", j - 1, "); ")
+		  			i--;
+		  			j--;
+				} else { // substitution operation
+		  			fmt.Println("(", i - 1, ", ", j - 1, "); ")
+		  			s_count++;
+		  			i--;
+		  			j--;
+				}
+		  	} else if diff == d[i][j] - d[i - 1][j] { // deletion operation
+				fmt.Println("(", i - 1, ", ", "-", "); ")
+				d_count++;
+				i--;
+		  	} else if diff == d[i][j] - d[i][j - 1] { // insertion operation
+				fmt.Println("(", "-", ", ", j - 1, "); ")
+				i_count++;
+				j--;
+		  	}
+		} else if i == 0 { // insertion operation
+		  fmt.Println("(","-", ", ", j - 1, "); ")
+		  i_count++;
+		  j--;	    
+		} else if j == 0 { // deletion operation
+		  fmt.Println("(", i - 1, ", ", "-", "); ")
+		  d_count++;
+		  i--;	
+		}    
+	}
+	return []int{i_count, d_count, s_count}
 }
--- distance_test.go
+++ distance_test.go
@@ -1,13 +1,29 @@
+//----------------------------------------------------------------------------------------
+// Copyright 2013 Nam S. Vo
+
+// Test for distance package
+//----------------------------------------------------------------------------------------
+
 package distance_test
 
 import (
-	"seq-cmp.2"
+	"distance2"
 	"fmt"
 	"runtime"
 	"strings"
 	"testing"
+	"math"
 )
 
+//Initialize constants
+var r_e float32 = 0.1
+var r_m float32 = 0.01
+
+var ERR_COST float32 = float32(math.Log10(float64(1/r_e))) // = 1
+
+var DIST_THRES int = 1000
+
+//Functions for displaying information
 func o_() string {
 	pc, _, _, _ := runtime.Caller(1)
 	name := runtime.FuncForPC(pc).Name()
@@ -20,127 +36,93 @@ func o_() string {
 
 func __(name string) {
 	fmt.Println("== END", name, "===")
+	fmt.Println()
 }
 
+type type_snpprofile map[int][][]byte
+type type_samelensnp map[int]int
+type TestCase struct {
+	Profile type_snpprofile
+	SNPlen type_samelensnp
+	genome string
+	read string
+	d float32
+}
 
-func TestEditDistanceStr(t *testing.T) {
+// Test for distance between reads and "starred" multi-genomes
+func TestDistanceMulti(t *testing.T) {
 	defer __(o_())
 
-	cases := [][]interface{}{
-		{"", "", 0},
-		{"ACGTACGT", "", 8},
-		{"", "ACGTACGT", 8},
-		{"ACGT", "ACGT", 0},
-		{"CGTA", "ACGT", 2},
-		{"TACGT", "ACGAT", 2},
-		{"GTTAC", "GTCAC", 1},
-		{"TCGACTCACCCTGCCCCGATTAACGTTGGACAGGAACCCTTGGTCTTCCGGCGAGCGGGCTTTTCACCCGCTTTATCGTTACTTATGTCAGCATTCGCAC",
-		 "TGCGAATGCTGACATAAGTAACGATAAAGCGGGTGAAAAGCCCGCTCGCCGGAAGACCAAGGGTTCCTGTCCAACGTTAATCGGGGCAGGGGGAGTCGAC", 52},
-		{"CTGAGTCTCTACCCCGGGTAGCTGCCCGGTTTAACATTCCCAGCCACAACACGGTTAAAAACTGGATAAAAGGCTACCGCAAATCTGGTAATGAGGCCTT",
-		 "AGGCCTCATTACCAGATTTGCGGTAGCCTTTTATCCAGTTTTTAACCGTGTTGTGGCTGGGAATGTTAAACCGGGCAGCTACCCGGGGTAGAGACTCAGA", 56},
-	}
+	var test_cases = []TestCase{
+		{ type_snpprofile{}, type_samelensnp{}, "ACG", "G", 0 },
+		{ type_snpprofile{}, type_samelensnp{}, "TTACG", "ACT", 1 },
 
-	for _, cs := range cases {
-		d := distance.EditDistanceStr(cs[0].(string), cs[1].(string))
-		if d != cs[2].(int) {
-			t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2].(int), d)
-		} // if
-	} // for case
+		{ type_snpprofile{3: {{'A'}, {'C'}} }, type_samelensnp{3: 1}, "ACC*CGT", "ACCACGT", 0 },
+		{ type_snpprofile{3: {{'A'}, {'C'}} }, type_samelensnp{3: 1}, "ACC*CGT", "ACCCCGA", 1 },
+		{ type_snpprofile{3: {{'A'}, {'C'}} }, type_samelensnp{3: 1}, "ACC*CGT", "CACGT", 0 },
+		{ type_snpprofile{3: {{'A'}, {'C'}} }, type_samelensnp{3: 1}, "ACC*CGT", "CGCGT", 1 },
+		{ type_snpprofile{3: {{'A'}, {'C'}} }, type_samelensnp{3: 1}, "ACC*CGT", "ACCTCGT", 1 },
+
+		{ type_snpprofile{3: {{'A'}, {'C'}, {'-'}} }, 	type_samelensnp{}, "ACC*CGT", "ACCACGT", 0 },
+		{ type_snpprofile{3: {{'A'}, {'C'}, {'-'}} }, 	type_samelensnp{}, "ACC*CGT", "ACCCCGT", 0 },
+		{ type_snpprofile{3: {{'A'}, {'C'}, {'-'}} }, 	type_samelensnp{}, "ACC*CGT", "ACCCGT", 0 },
+		{ type_snpprofile{3: {{'A'}, {'C'}, {'-'}} }, 	type_samelensnp{}, "ACC*CGT", "ACCTCGT", 1 },
+
+		{ type_snpprofile{3: {{'A'}, {'A','C'}} }, type_samelensnp{}, "ACC*CGT", "ACCACGT", 0 },
+		{ type_snpprofile{3: {{'A'}, {'A','C'}} }, type_samelensnp{}, "ACC*CGT", "ACCACCGT", 0 },
+		{ type_snpprofile{3: {{'A'}, {'A','C'}} }, type_samelensnp{}, "ACC*CGT", "ACCATCGT", 1 },
+
+		{ type_snpprofile{3: {{'A'}, {'A','C'}} }, type_samelensnp{}, "ACC*CGT", "ACCTCGT", 1 },
+		{ type_snpprofile{3: {{'A'}, {'A','C'}} }, type_samelensnp{}, "ACC*CGT", "ACCACCGT", 0 },
+		{ type_snpprofile{3: {{'A'}, {'A','C'}} }, type_samelensnp{}, "ACC*CGT", "ACCTCCGT", 1 },
+
+		{ type_snpprofile{3: {{'T'}, {'T', 'T', 'A'}} }, type_samelensnp{}, "ACC*CGT", "ACCTCGT", 0 },
+		{ type_snpprofile{3: {{'T'}, {'T', 'T', 'A'}} }, type_samelensnp{}, "ACC*CGT", "ACCTTACGT", 0 },
+		{ type_snpprofile{3: {{'T'}, {'T', 'T', 'A'}} }, type_samelensnp{}, "ACC*CGT", "ACCGTACGT", 1 },
+		{ type_snpprofile{3: {{'T'}, {'T', 'T', 'A'}} }, type_samelensnp{}, "ACC*CGT", "ACCTACGT", 2 },
+		{ type_snpprofile{3: {{'T'}, {'T', 'T', 'A'}} }, type_samelensnp{}, "ACC*CGT", "ACCTTCGT", 3 },
+		{ type_snpprofile{3: {{'T'}, {'T', 'T', 'A'}} }, type_samelensnp{}, "ACC*CGT", "ACCTTCGG", 4 },
+
+		{ type_snpprofile{3: {{'A'}, {'C'}} }, type_samelensnp{3: 1}, "ACC*CGT", "TTTACCACGT", float32(distance.INF) },
+		{ type_snpprofile{3: {{'A'}, {'C'}} }, type_samelensnp{3: 1}, "ACC*CGT", "TTTACCACGT", float32(distance.INF) },
+
+		{ type_snpprofile{3: {{'A'}, {'T', 'A'}, {'T', 'T', 'A'}} }, type_samelensnp{}, "ACC*CGT", "ACCGTACGT", 1 },
+		{ type_snpprofile{3: {{'A'}, {'T', 'A'}, {'T', 'T', 'A'}, {'-'}} }, type_samelensnp{}, "ACC*CGT", "ACCTTACGT", 0 },
+		{ type_snpprofile{3: {{'A'}, {'T', 'A'}, {'T', 'T', 'A'}, {'-'}} }, type_samelensnp{}, "ACC*CGT", "ACCGTACGT", 1 },
+
+		{ type_snpprofile{0: {{'A'}, {'T', 'A'}, {'T', 'T', 'A'}, {'-'}} }, type_samelensnp{}, "*ACGT", "TTAACGT", 0 },
+		{ type_snpprofile{0: {{'A'}, {'T', 'A'}, {'T', 'T', 'A'}, {'-'}} }, type_samelensnp{}, "*ACGT", "GTAACGT", 1 },
+		{ type_snpprofile{0: {{'A'}, {'T', 'A'}, {'T', 'T', 'A'}, {'-'}} }, type_samelensnp{}, "*ACGT", "ATACGT", 2 },
+
+	}
+	for i := 0; i < len(test_cases); i++ {
+		distance.Init(ERR_COST, DIST_THRES, test_cases[i].Profile, test_cases[i].SNPlen)
+		//fmt.Println(">", distance.ERR_COST, distance.INF, distance.DIST_THRES, distance.SNP_PROFILE, distance.SNP_LEN)
+		dis, d, m, n, _ := distance.DistanceMulti([]byte(test_cases[i].read), []byte(test_cases[i].genome), 0)
+		if dis != test_cases[i].d {
+			t.Errorf("Fail alignment (case, calculated distance, true distance):", i, string([]byte(test_cases[i].read)), string([]byte(test_cases[i].genome)), dis, test_cases[i].d, d, m, n)
+		} else {
+			fmt.Println("Successful alignment (read, genome, distance): ", test_cases[i].read, test_cases[i].genome, test_cases[i].d)
+		}
+	}
 }
 
-func TestHammingDistance(t *testing.T) {
+/*
+// Test for distance between reads and "starred" multi-genomes
+func TestDistanceMultiSpeed(t *testing.T) {
 	defer __(o_())
 
-	cases := [][]interface{}{
-		{"", "", 0},
-		{"AGCBBHTYHHHWDWD", "", 0},
-		{"", "AGCBBHTYHHHWDWD", 0},
-
-		{"AGCCTTCAATATT", "AGCbBHyHHWdWD", 0},
-		{"AGCCATCAATATT", "ATCBBHYHHWDWD", 4},
-		{"AGCCTTCAATATC", "AGCBBHYHHWDWD", 3},
-		{"AGCCGTCAATATT", "GGCBBhYHhWDwD", 1},
-		{"AGCCATCATAT"  , "AGCBbHYHHwDWD", 3},
-
-		{"TCGACTCACCCTGCCCCGATTAACGTTGGACAGGAACCCTTGGTCTTCCGGCGAGCGGGCTTTTCACCCGCTTTATCGTTACTTATGTCAGCATTCGCAC",
-		 "TGCGAATGCTGRCATAAGTAACGABAAAGCGGGTGAAAAGCCCGCTCGCCGGAAGACHAAGGGTTCCTGTCCAACGTTAATCGGGGCAGGGGGAGTCGAV", 104},
-
-		{"CTGAGTCTCTACCCCGGGTAGCTGCCCGGTTTAACATTCCCAGCCACAACACGGTTAAAAACTGGATAAAAGGCTACCGCAAATCTGGTAATGAGGCCTT",
-		 "AGGCCTCRTTACCAGATTTKCGGTAGCCTTTTATCCABTTTTTAACCGTGTTGTGGCTGGGHATGTTAAACCGGGCAGCTACCCGGGGTAGAGACTCAGA", 82},
+	var test_cases = []TestCase{
+		{ type_profile{32: {{'A'}, {'T', 'A'}, {'T', 'T', 'A'}, {'-'}} }, type_samelensnp{32: 0}, "ACGTACGTACGTACGTACGTACGTACGTACGT*", "ACGTACGTACGTACGTACGTACGTACGTACGTAT", 2 },
 	}
-
-	for _, cs := range cases {
-		d := distance.HammingDistance([]byte(cs[0].(string)), []byte(cs[1].(string)))
-		if d != float32(cs[2].(int)) {
-			t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2].(int), d)
-		} // if
-	} // for case
-}
-
-func TestEditDistanceArray(t *testing.T) {
-	defer __(o_())
-
-	cases := [][]interface{}{
-		{"", "", 0},
-		{"AGCBBHTYHHHWDWD", "", distance.DIST_THRES + 1},
-		{"", "AGCBBHTYHHHWDWD", int(3.2212255e+10)},
-
-		{"AGCCTTCAATATT", "AGCBBHtYhHHWDWD", 2},
-		{"AGCCATCAATATT", "AGCBBHtYhHHWDWD", 5},
-		{"AGCCTTCAATATC", "AGCBBHtYhHHWDWD", 5},
-		{"AGCCGTCAATATT", "AGCBBHtYhHHWDWD", 2},
-		{"AGCCATCATATT" , "AGCBBHtYhHHWDWD", int(2.1474836e+09)},
-
-		{"TCGACTCACCCTGCCCCGATTAACGTTGGACAGGAACCCTTGGTCTTCCGGCGAGCGGGCTTTTCACCCGCTTTATCGTTACTTATGTCAGCATTCGCAC",
-		 "TGCGAATGCTGRCATAAGTAACGABAAAGCGGGTGAAAAGCCCGCTCGCCGGAAGACHAAGGGTTCCTGTCCAACGTTAATCGGGGCAGGGGGAGTCGAV", 104},
-
-		{"CTGAGTCTCTACCCCGGGTAGCTGCCCGGTTTAACATTCCCAGCCACAACACGGTTAAAAACTGGATAAAAGGCTACCGCAAATCTGGTAATGAGGCCTT",
-		 "AGGCCTCRTTACCAGATTTKCGGTAGCCTTTTATCCABTTTTTAACCGTGTTGTGGCTGGGHATGTTAAACCGGGCAGCTACCCGGGGTAGAGACTCAGA", 82},
+	k:=1000000
+	for i := 0; i < len(test_cases); i++ {
+		distance.Init(ERR_COST, DIST_THRES, test_cases[i].Profile, test_cases[i].SNPlen)
+		//fmt.Println(">", distance.ERR_COST, distance.INF, distance.DIST_THRES, distance.SNP_PROFILE, distance.SNP_LEN)
+		for j:=0; j < k; j++ {
+			distance.DistanceMulti([]byte(test_cases[i].read), []byte(test_cases[i].genome), 0)
+		}
+		fmt.Println("Successful alignment for ", k, "reads")
 	}
-
-	fmt.Println("\tSNP_SUB_COST: ", distance.SNP_SUB_COST)
-	fmt.Println("\tN_SNP_SUB_COST: ", distance.N_SNP_SUB_COST)
-	fmt.Println("\tINF: ", distance.INF)
-	fmt.Println("\tDIST_THRES: ", distance.DIST_THRES)
-
-	for _, cs := range cases {
-		d := distance.EditDistanceArray([]byte(cs[0].(string)), []byte(cs[1].(string)))
-		if d != float32(cs[2].(int)) {
-			t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2].(int), d)
-		} // if
-	} // for case
 }
-
-func TestEditDistanceMatrix(t *testing.T) {
-	defer __(o_())
-
-	cases := [][]interface{}{
-		{"", "", 0},
-		{"AGCBBHTYHHHWDWD", "", distance.DIST_THRES + 1},
-		{"", "AGCBBHTYHHHWDWD", int(3.2212255e+10)},
-
-		{"AGCCTTCAATATT", "AGCBBHtYhHHWDWD", 2},
-		{"AGCCATCAATATT", "AGCBBHtYhHHWDWD", 5},
-		{"AGCCTTCAATATC", "AGCBBHtYhHHWDWD", 5},
-		{"AGCCGTCAATATT", "AGCBBHtYhHHWDWD", 2},
-		{"AGCCATCATATT" , "AGCBBHtYhHHWDWD", int(2.1474836e+09)},
-
-		{"TCGACTCACCCTGCCCCGATTAACGTTGGACAGGAACCCTTGGTCTTCCGGCGAGCGGGCTTTTCACCCGCTTTATCGTTACTTATGTCAGCATTCGCAC",
-		 "TGCGAATGCTGRCATAAGTAACGABAAAGCGGGTGAAAAGCCCGCTCGCCGGAAGACHAAGGGTTCCTGTCCAACGTTAATCGGGGCAGGGGGAGTCGAV", 104},
-
-		{"CTGAGTCTCTACCCCGGGTAGCTGCCCGGTTTAACATTCCCAGCCACAACACGGTTAAAAACTGGATAAAAGGCTACCGCAAATCTGGTAATGAGGCCTT",
-		 "AGGCCTCRTTACCAGATTTKCGGTAGCCTTTTATCCABTTTTTAACCGTGTTGTGGCTGGGHATGTTAAACCGGGCAGCTACCCGGGGTAGAGACTCAGA", 82},
-	}
-
-	fmt.Println("\tSNP_SUB_COST: ", distance.SNP_SUB_COST)
-	fmt.Println("\tN_SNP_SUB_COST: ", distance.N_SNP_SUB_COST)
-	fmt.Println("\tINF: ", distance.INF)
-	fmt.Println("\tDIST_THRES: ", distance.DIST_THRES)
-
-	for _, cs := range cases {
-		d := distance.EditDistanceMatrix([]byte(cs[0].(string)), []byte(cs[1].(string)))
-		if d != float32(cs[2].(int)) {
-			t.Errorf("Edit-distance between %s and %s is expected to be %d, but %d got!", cs[0], cs[1], cs[2].(int), d)
-		} // if
-	} // for case
-}
+*/
